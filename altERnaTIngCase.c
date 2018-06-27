3b915467df83f5e3647a8529c31e17047dd73558
Nicholas Hoover <n.hoover13@gmail.com>
2018-06-26 18:04:40 -0700

    work

--- altERnaTIngCase.c
+++ altERnaTIngCase.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
-
-
-
-
 char* to_alternating_case(const char *s) {
+  char turnString[strlen(s)];
+
   for (int i = 0; i < strlen(s); i++){
-      printf("%c\n",s[i] -39);
-
-
-
+    if(s[i]>96 && s[i]<123 ){
+      //printf("%c\n",s[i] -32);
+      turnString[i] = s[i] -32;
+    }
+    else if(s[i]>64 && s[i]<91 ){
+      //printf("%c\n",s[i] +32);
+      turnString[i] = s[i] +32;
+    }
+    else{
+      //printf("%c\n",s[i]);
+      turnString[i] = s[i];
+    }
   }
 
+  printf("%s\n",turnString);
+  printf("%s\n","HELLO WORLD");
 
-  return ""; // TODO
+
+  return turnString; // TODO
 }
 
 
 int main()
 {
-   to_alternating_case("a");
+   printf("%s\n", to_alternating_case("hello world"));
+   printf("%s\n", to_alternating_case("hello world") == "HELLO WORLD");
 
 
    return 0;
